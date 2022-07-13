9f6231d8bbab16e9fbd251394d0c6f19711628bd
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2022-07-13 15:30:38 +0200

    fixed scale of bezier curve

--- client/components/BezierCurve.js
+++ client/components/BezierCurve.js
@@ -113,7 +113,7 @@ const CubicBezier = () => {
 	}, [intermediatePoints1]);
 
 	return (
-		<svg xmlns='http://www.w3.org/2000/svg' width='full' height='100%' viewBox='0 0 850 600'>
+		<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox='0 0 850 600'>
 			{intermediatePoints1.length === 3 && (
 				<path
 					d={`M ${controlPoints[0].x} ${controlPoints[0].y}  c ${controlPoints[1].x - controlPoints[0].x} ${controlPoints[1].y - controlPoints[0].y} ${
